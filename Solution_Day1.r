d23a3417dd6d852fc9f1c37caf7b9208c1a08ab0
sommergabriel <76773054+sommergabriel@users.noreply.github.com>
2020-12-31 16:18:19 +0100

    Add files via upload

---
+++ Solution_Day1.r
@@ -0,0 +1,32 @@
+mydata <- read.delim(file="input.txt", header = FALSE, sep = "\n")
+numbers <- as.vector(t(mydata))
+str(numbers)
+
+# ----- Part1 ----- #
+
+for (i in numbers) {
+  for (j in numbers) {
+    if ( i+j == 2020) {
+      print(i)
+      print(j)
+      print(i*j)
+    }
+  }
+}
+
+# ----- Part2 ----- #
+
+for (i in numbers) {
+  for (j in numbers) {
+    for (k in numbers) {
+      if (i+j+k == 2020) {
+        print(i)
+        print(j)
+        print(k)
+        print(i*j*k)
+      }
+    }
+  }
+}
+
+# ----- The End ----- #
